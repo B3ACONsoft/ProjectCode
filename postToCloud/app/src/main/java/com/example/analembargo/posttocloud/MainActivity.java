3a40ad2bbd22dc18a9cd3a947ecf9e52d3ad55fc
wakeTechN00b <dhjohnson3@my.waketech.edu>
2016-02-03 23:19:48 -0500

    Added comments to MainActivity.java

--- postToCloud/app/src/main/java/com/example/analembargo/posttocloud/MainActivity.java
+++ postToCloud/app/src/main/java/com/example/analembargo/posttocloud/MainActivity.java
@@ -31,8 +31,6 @@ public class MainActivity extends AppCompatActivity {
     //this is url i'm posting data to
     private static final String postURL = "http://capstone1.netai.net/devDave/postDemo.php";
     //private static final String postURL = "http://172.20.23.100/postDemo.php";
-    //this is the url where you can view the changes you're making
-    private static final String displayURL = "capstone1.netai.net/devDave/display.php";
 
     //these are the parameters for the select commmand
     private static final Map<String, String> selectValues;
@@ -45,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    //these are the parameters for the insert command
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,7 +83,12 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /*
+        This takes the values from the text fields, and packs them up as
+        key-value pairs in a HashMap.
 
+        It then performs an http Post to insert the key-value pairs
+     */
     public void insertDemo(View v)
     {
         HashMap<String, String> insertValues = new HashMap<String, String>();
@@ -106,11 +109,17 @@ public class MainActivity extends AppCompatActivity {
         new postRequestTask().execute(parseParams((HashMap<String, String>)insertValues));
     }
 
+    /*
+        This performs a select * from coaches
+     */
     public void selectDemo(View v)
     {
         new postRequestTask().execute(parseParams((HashMap<String, String>)selectValues));
     }
 
+    /*
+        updates the results text field
+     */
     public void updateTextField(String update) {
         EditText editText = (EditText)findViewById(R.id.tbResult);
         editText.setText("");
@@ -118,6 +127,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    /*
+        This formats the key-values pairs correctly for the
+        Post request in the format:
+        key1=value1&key2=value2 etc etc
+
+        Post requests key- value pair must be separated by  "&"
+     */
     private String parseParams(HashMap<String, String> paramsMap)
     {
         StringBuilder paramsBuilder = new StringBuilder();
@@ -139,6 +155,11 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    /*
+        This is function fires a thread which makes HTTP POST Request
+        and gets the response, and updates the result text area with it.
+
+     */
     private class postRequestTask extends AsyncTask<String, Void, String> {
 
         //these are the http connection objects
@@ -150,29 +171,31 @@ public class MainActivity extends AppCompatActivity {
             return postRequest(postURL, params[0]);
         }
 
+        //make a post request
         public String postRequest(String url, String params){
             StringBuilder responseBuffer = new StringBuilder();
             try
             {
-                byte[] bytes = params.getBytes();
+                byte[] bytes = params.getBytes();   //convert the data to bytes
 
                 this.url = new URL(url);
-                conn = (HttpURLConnection) this.url.openConnection();
+                conn = (HttpURLConnection) this.url.openConnection();   //init the connection objects
                 conn.setDoInput(true);
                 conn.setDoInput(true);
                 conn.setInstanceFollowRedirects(false);
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                conn.setRequestProperty("charset", "utf-8");
-                conn.setRequestProperty("Content-Length", Integer.toString(bytes.length));
-                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("POST");                          //set request method
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");       //set the attributes of the HTTP header
+                conn.setRequestProperty("charset", "utf-8");                                        //more attributes...
+                conn.setRequestProperty("Content-Length", Integer.toString(bytes.length));          //more attributes...
+                conn.setConnectTimeout(10000);                                                      //set the timeout if things break
 
-                DataOutputStream dataOutputStream = new DataOutputStream(conn.getOutputStream());
-                dataOutputStream.write(bytes);
+                DataOutputStream dataOutputStream = new DataOutputStream(conn.getOutputStream());   //get output stream from websocket
+                dataOutputStream.write(bytes);                                                      //write data across the socket
                 dataOutputStream.flush();
                 dataOutputStream.close();
 
-                InputStreamReader reader = new InputStreamReader(conn.getInputStream());
+
+                InputStreamReader reader = new InputStreamReader(conn.getInputStream());            //get the response
                 int input;
                 while((input = reader.read()) != -1)
                 {
@@ -184,13 +207,13 @@ public class MainActivity extends AppCompatActivity {
             } finally {
                 conn.disconnect();
             }
-            return responseBuffer.toString();
+            return responseBuffer.toString();                                                       //pass the response to onPostExecute()
         }
 
         @Override
         protected  void onPostExecute(String result) {
             super.onPostExecute(result);
-            updateTextField(result);
+            updateTextField(result);                                                                //show result
         }
     }
 }
