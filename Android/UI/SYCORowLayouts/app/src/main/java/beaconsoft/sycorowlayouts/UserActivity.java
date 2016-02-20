f4058c4af7744ad9c20531236387626fd8da372b
ptcurrin <ptcurrin@my.waketech.edu>
2016-02-20 18:43:06 -0500

    UserActivity Quick and Dirty View Alignment on Test ListView

--- Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/UserActivity.java
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/UserActivity.java
@@ -26,16 +26,16 @@ public class UserActivity extends AppCompatActivity {
         DBHelper helper = new DBHelper(this);
         String query;
 
-//        query = "INSERT INTO users VALUES ('1', 'PATRICK', 'CURRIN', '(919)693-2121', '(919)693-8011', 'CURRIN.PATRICK@YAHOO.COM', 'ADMIN')";
-//        helper.getWritableDatabase().execSQL(query);
-//        query = "INSERT INTO users VALUES ('2', 'KATY', 'PERRY', '(919)345-6543', '(919)456-7894', 'KATY@PERRY.COM', 'USER')";
-//        db.execSQL(query);
+        query = "INSERT INTO users VALUES ('1', 'PATRICK', 'CURRIN', '(919)693-2121', '(919)693-8011', 'CURRIN.PATRICK@YAHOO.COM', 'ADMIN')";
+        helper.getWritableDatabase().execSQL(query);
+        query = "INSERT INTO users VALUES ('2', 'KATY', 'PERRY', '(919)345-6543', '(919)456-7894', 'KATY@PERRY.COM', 'USER')";
+        helper.getWritableDatabase().execSQL(query);
 
 
         rosterList = helper.getSQLiteUsers();
         ListView testView = (ListView)findViewById(R.id.listViewUserActivityTest);
         ListAdapter adapter = new SimpleAdapter(UserActivity.this, rosterList, R.layout.list_view_users_row_layout,
-                new String[] {  "fname", "lname", "phone", "emergency", "email", "user_type" },
+                new String[] {  "fname", "lname", "phone" },
                 new int[]    {  R.id.testfname, R.id.testlname, R.id.testphone});
         testView.setVisibility(View.VISIBLE);
         testView.setAdapter(adapter);
