e4e4ec61ae3733d331bad250d5db204c52daef4f
ptcurrin <ptcurrin@my.waketech.edu>
2016-02-20 18:48:14 -0500

    Changed Activity Name to UserRosterActivity

--- Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/UserRosterActivity.java
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/UserRosterActivity.java
@@ -1,7 +1,6 @@
 package beaconsoft.sycorowlayouts;
 
 import android.app.ProgressDialog;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +11,7 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class UserActivity extends AppCompatActivity {
+public class UserRosterActivity extends AppCompatActivity {
 
     private DBHelper helper = new DBHelper(this);
     private ArrayList<HashMap<String, String>> rosterList;
@@ -21,7 +20,7 @@ public class UserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_user);
+        setContentView(R.layout.activity_user_roster);
 
         DBHelper helper = new DBHelper(this);
         String query;
@@ -34,7 +33,7 @@ public class UserActivity extends AppCompatActivity {
 
         rosterList = helper.getSQLiteUsers();
         ListView testView = (ListView)findViewById(R.id.listViewUserActivityTest);
-        ListAdapter adapter = new SimpleAdapter(UserActivity.this, rosterList, R.layout.list_view_users_row_layout,
+        ListAdapter adapter = new SimpleAdapter(UserRosterActivity.this, rosterList, R.layout.list_view_users_row_layout,
                 new String[] {  "fname", "lname", "phone" },
                 new int[]    {  R.id.testfname, R.id.testlname, R.id.testphone});
         testView.setVisibility(View.VISIBLE);
