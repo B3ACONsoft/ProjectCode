f3b626cd64e360407ce86f3fcb01ac73ae3064a3
ptcurrin <ptcurrin@my.waketech.edu>
2016-03-09 00:15:18 -0500

    DOA Approach, 2 Major Classes Set up for Object Manipulation

---
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/Enrollment.java
@@ -0,0 +1,78 @@
+package beaconsoft.sycorowlayouts;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by Patrick on 3/8/2016.
+ */
+public class Enrollment {
+
+    private int enrollmentID;
+    private int userID;
+    private int playerID;
+    private int leagueID;
+    private int teamID;
+    private Date enrollmentDate;
+    private BigDecimal fee;
+
+    public int getEnrollmentID() {
+        return enrollmentID;
+    }
+
+    public void setEnrollmentID(int enrollmentID) {
+        this.enrollmentID = enrollmentID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public int getLeagueID() {
+        return leagueID;
+    }
+
+    public void setLeagueID(int leagueID) {
+        this.leagueID = leagueID;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public Date getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(Date enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
+
+    public BigDecimal getFee() {
+        return fee;
+    }
+
+    public void setFee(BigDecimal fee) {
+        this.fee = fee;
+    }
+
+    public String toString(){
+        return enrollmentID + " " + userID + " " + fee;
+    }
+}
---
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/Team.java
@@ -0,0 +1,49 @@
+package beaconsoft.sycorowlayouts;
+
+/**
+ * Created by Patrick on 3/8/2016.
+ */
+public class Team {
+
+    private int teamID;
+    private String teamName;
+    private int leagueID;
+    private int userID;
+
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getLeagueID() {
+        return leagueID;
+    }
+
+    public void setLeagueID(int leagueID) {
+        this.leagueID = leagueID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String toString(){
+        return teamID + " " + teamName;
+    }
+}
