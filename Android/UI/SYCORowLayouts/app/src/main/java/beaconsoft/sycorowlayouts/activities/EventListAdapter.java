9a6f1064239d86ce9caa54905d75a19275292d05
ptcurrin <ptcurrin@my.waketech.edu>
2016-03-22 11:25:05 -0400

    Calendar Listview fixes

--- Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/activities/EventListAdapter.java
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/activities/EventListAdapter.java
@@ -1,25 +1,27 @@
 package beaconsoft.sycorowlayouts.activities;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
-
 import java.util.List;
-
+import beaconsoft.sycorowlayouts.DataSource;
 import beaconsoft.sycorowlayouts.R;
 import beaconsoft.sycorowlayouts.dbobjects.Event;
+import beaconsoft.sycorowlayouts.dbobjects.Place;
 
 public class EventListAdapter extends ArrayAdapter<Event>{
-
+	private DataSource dataSource;
 	private List<Event> data = null;
 	private static LayoutInflater inflater=null;
-	
-	public EventListAdapter(Context context, int layoutResourceId, List<Event> data) {
+
+	public EventListAdapter(Context context, int layoutResourceId, List<Event> data, DataSource dataSource) {
 		super(context, layoutResourceId, data);
 		this.data = data;
+		this.dataSource = dataSource;
 	}
 	
 		@Override
@@ -32,10 +34,12 @@ public class EventListAdapter extends ArrayAdapter<Event>{
 		
 		TextView type = (TextView)row.findViewById(R.id.notificationType);
 		TextView place = (TextView)row.findViewById(R.id.notificationPlace);
-		
-		Event e = data.get(position);
-		type.setText(e.getEventType());
-		place.setText(e.getPlaceID());
+
+			Event e = data.get(position);
+			Place p = dataSource.getPlaceById(e.getPlaceID());
+		type.setText(e.getEventType() + " " + e.getStartDateTime());
+		place.setText(p.getPlaceName() + "\n" + p.getStreetAddress() + " " + p.getCity() + ", " +
+				p.getState() + " " + p.getZip());
 		
 		return row;
 	}
