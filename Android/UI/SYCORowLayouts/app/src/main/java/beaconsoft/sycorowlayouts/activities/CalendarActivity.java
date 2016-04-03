89695b0cbe8a6f492eb97b5e3090cdea4709a017
ptcurrin <ptcurrin@my.waketech.edu>
2016-04-03 18:11:32 -0400

    Lost of Changes,
    Calendar is completely different because Jolichelle got it going in the
    right direction with a custom calednar widget!!!!

--- Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/activities/CalendarActivity.java
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/activities/CalendarActivity.java
@@ -4,112 +4,160 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ListAdapter;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.CalendarView;
-import android.widget.ListView;
-import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Locale;
-import java.util.TimeZone;
-import beaconsoft.sycorowlayouts.DataSource;
-import beaconsoft.sycorowlayouts.EventListAdapter;
+
 import beaconsoft.sycorowlayouts.R;
-import beaconsoft.sycorowlayouts.dbobjects.Event;
+import beaconsoft.sycorowlayouts.activities.adapter.CalendarAdapter;
+import beaconsoft.sycorowlayouts.activities.util.CalendarCollection;
+
+import java.util.GregorianCalendar;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    //I am writing something in the Calendar Activity
+    public GregorianCalendar cal_month, cal_month_copy;
+    private CalendarAdapter cal_adapter;
+    private TextView tv_month;
 
-    private TextView eventTextPrompt;
-    private int currentAdminId;
-    private int currentLeagueId;
-    private int currentTeamId;
-    private String currentAdminEmail;
-    private String currentAdminName;
     private static final String   NAME_KEY = "beaconsoft.sycorowlayouts.NAME";
     private static final String  ADMIN_KEY = "beaconsoft.sycorowlayouts.ADMIN";
     private static final String  EMAIL_KEY = "beaconsoft.sycorowlayouts.EMAIL";
-    private static final String   TEAM_KEY = "beaconsoft.sycorowlayouts.TEAM";
     private static final String LEAGUE_KEY = "beaconsoft.sycorowlayouts.LEAGUE";
-    private List<Event> eventList = new ArrayList<Event>();
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd", Locale.US);
-    GregorianCalendar cal = new GregorianCalendar(TimeZone.getTimeZone("US/Central"));
-    private DataSource datasource;
+    private static final String   TEAM_KEY = "beaconsoft.sycorowlayouts.TEAM";
+
+    private int currentTeamId;
+    private int currentAdminId;
+    private int currentLeagueId;
+    private String name;
+    private String currentAdminEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
-        datasource = new DataSource(this);
-        try {
-            datasource.open();
-        } catch (SQLException e) {
-            e.printStackTrace();
+
+        Intent intent = getIntent();
+        currentLeagueId = intent.getIntExtra(LEAGUE_KEY, 0);
+        currentAdminId = intent.getIntExtra(ADMIN_KEY, 0);
+        currentAdminEmail = intent.getStringExtra(EMAIL_KEY);
+        name = intent.getStringExtra(NAME_KEY);
+        currentTeamId = intent.getIntExtra(TEAM_KEY, 0);
+
+        cal_month = (GregorianCalendar) GregorianCalendar.getInstance();
+        cal_month_copy = (GregorianCalendar) cal_month.clone();
+        cal_adapter = new CalendarAdapter(this, cal_month, /* What is this? */ CalendarCollection.date_collection_arr);
+
+
+
+        tv_month = (TextView) findViewById(R.id.tv_month);
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
+
+        ImageButton previous = (ImageButton) findViewById(R.id.ib_prev);
+
+        previous.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setPreviousMonth();
+                refreshCalendar();
+            }
+        });
+
+        ImageButton next = (ImageButton) findViewById(R.id.Ib_next);
+        next.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                setNextMonth();
+                refreshCalendar();
+
+            }
+        });
+
+        GridView gridview = (GridView) findViewById(R.id.gv_calendar);
+        gridview.setAdapter(cal_adapter);
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+                String selectedGridDate = CalendarAdapter.day_string
+                        .get(position);
+
+                String[] separatedTime = selectedGridDate.split("-");
+                String gridvalueString = separatedTime[2].replaceFirst("^0*","");
+                int gridvalue = Integer.parseInt(gridvalueString);
+
+                if ((gridvalue > 10) && (position < 8)) {
+                    setPreviousMonth();
+                    refreshCalendar();
+                } else if ((gridvalue < 7) && (position > 28)) {
+                    setNextMonth();
+                    refreshCalendar();
+                }
+                ((CalendarAdapter) parent.getAdapter()).setSelected(v,position);
+
+                ((CalendarAdapter) parent.getAdapter()).getPositionList(selectedGridDate, CalendarActivity.this);
+            }
+
+        });
+    }
+
+    //TODO: --> This stuff first - Put in Datasource, and replace your listview adapter with mine, which is really
+    //just the list_view and adapter you sent me 3 weeks ago debugged, "beaconsoft.sycorowlayouts.EventListAdpater.java
+    //and the xml file list_view_event_items.xml
+
+    //TODO: --> This stuff second, for sure - short click on a date reconstitutes the listview,
+    // Clear the arraylist, repopulate the arraylist, and pass into a new adapter
+
+    //TODO: --> maybe not at all (MAYBE) long-click, make a new intent and pass the date
+
+
+    protected void setNextMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMaximum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) + 1),
+                    cal_month.getActualMinimum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) + 1);
         }
 
-       /*Okay Jolichelle, I've got your intent extras passing in the current team and league, and admin id */
-        Intent intent = getIntent();
-        currentAdminEmail = intent.getStringExtra(EMAIL_KEY);
-        currentAdminName  = intent.getStringExtra(NAME_KEY);
-        currentAdminId    = intent.getIntExtra(ADMIN_KEY, 0);
-        currentLeagueId   = intent.getIntExtra(LEAGUE_KEY, 0);
-        currentTeamId     = intent.getIntExtra(TEAM_KEY, 0);
+    }
 
-        final CalendarView c = (CalendarView) findViewById(R.id.calendarView1);
-        cal.setTimeInMillis(c.getDate());
-        //eventList = datasource.getListOfEvents(Integer.parseInt(sdf.format(cal.getTime())));
-        eventList = datasource.getListOfEvents();
-        ListAdapter adapter = new EventListAdapter(getApplicationContext(), R.layout.notification_list_item, eventList, datasource);
-        ListView listview = (ListView) findViewById(R.id.eventList);
-        listview.setAdapter(adapter);
-//        c.setOnDateChangeListener(new OnDateChangeListener() {
-//
-//            @Override
-//            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-//                cal.setTimeInMillis(c.getDate());
-//                //eventList = datasource.getListOfEvents(Integer.parseInt(sdf.format(cal.getTime())));
-//                eventList = datasource.getListOfEvents();
-//                ListAdapter adapter = new EventListAdapter(CalendarActivity.this, R.layout.notification_list_item, eventList);
-//                ListView listview = (ListView) findViewById(R.id.eventList);
-//                listview.setAdapter(adapter);
-//            }
-//        });
+    protected void setPreviousMonth() {
+        if (cal_month.get(GregorianCalendar.MONTH) == cal_month
+                .getActualMinimum(GregorianCalendar.MONTH)) {
+            cal_month.set((cal_month.get(GregorianCalendar.YEAR) - 1),
+                    cal_month.getActualMaximum(GregorianCalendar.MONTH), 1);
+        } else {
+            cal_month.set(GregorianCalendar.MONTH,
+                    cal_month.get(GregorianCalendar.MONTH) - 1);
+        }
+
+    }
+
+    public void refreshCalendar() {
+        cal_adapter.refreshDays();
+        cal_adapter.notifyDataSetChanged();
+        tv_month.setText(android.text.format.DateFormat.format("MMMM yyyy", cal_month));
     }
 
     public void goToEditEvents(View view){
-        Intent intent = new Intent(getApplicationContext(), EditEventsActivity.class);
-        intent.putExtra(NAME_KEY, currentAdminName);
-        intent.putExtra(ADMIN_KEY, currentAdminId);
+        Intent intent = new Intent(this, EditEventsActivity.class);
         intent.putExtra(LEAGUE_KEY, currentLeagueId);
-        intent.putExtra(TEAM_KEY, currentTeamId);
+        intent.putExtra(NAME_KEY, name);
+        intent.putExtra(ADMIN_KEY, currentAdminId);
         intent.putExtra(EMAIL_KEY, currentAdminEmail);
+        intent.putExtra(TEAM_KEY, currentTeamId);
         startActivity(intent);
     }
 
-
-    @Override
-    protected void onDestroy(){
-        datasource.close();
-        super.onDestroy();
-    }
-
-    @Override
-    protected void onResume() {
-        try {
-            datasource.open();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        datasource.close();
-        super.onPause();
-    }
 }
+
+
