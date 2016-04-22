a22a471e26b1187c9af0df5839928cb921abdd2e
wakeTechN00b <dhjohnson3@my.waketech.edu>
2016-04-21 23:35:05 -0400

    added new server access

---
+++ Android/UI/SYCORowLayouts/app/src/main/java/beaconsoft/sycorowlayouts/SYCOServerAccess/RemoteSelectOperations.java
@@ -0,0 +1,8 @@
+package beaconsoft.sycorowlayouts.SYCOServerAccess;
+
+
+public class RemoteSelectOperations extends RemoteOperations {
+    public RemoteSelectOperations(RemoteConnection remoteConnection) {
+        super(remoteConnection);
+    }
+}
