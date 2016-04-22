c17ae53782025fb4319e832d53085d0c887a06a1
wakeTechN00b <dhjohnson3@my.waketech.edu>
2016-04-21 23:33:05 -0400

    delete old server access

--- Android/UI/SYCORowLayouts/app/src/main/java/SYCOServerAccess/RemoteSelectOperations.java
+++
@@ -1,8 +0,0 @@
-package SYCOServerAccess;
-
-
-public class RemoteSelectOperations extends RemoteOperations {
-    public RemoteSelectOperations(RemoteConnection remoteConnection) {
-        super(remoteConnection);
-    }
-}
